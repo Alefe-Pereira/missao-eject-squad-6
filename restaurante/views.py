633612b80b6e1b253fb0dda7161d1df548a62d89
PedroGH96 <pdalves30@gmail.com>
2026-04-10 16:32:57 -0300

    feat: adicionar template dinamico da homepage com destaques

--- restaurante/views.py
+++ restaurante/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ArtigoBlog, Reserva, Destaque
 
-# Create your views here.
+def home(request):
+    destaques = Destaque.objects.all()
+    return render(request, 'index.html', {'destaques': destaques})
+
+def blog(request):
+    artigos = ArtigoBlog.objects.all()[:4]
+    return render(request, 'blog/blog.html', {'artigos': artigos})
+
+def artigo(request, id):
+    artigo = ArtigoBlog.objects.get(id=id)
+    return render(request, 'blog/artigo.html', {'artigo': artigo})
+
+def sobre(request):
+    return render(request, 'sobre.html')
+
+def criar_reserva(request):
+    if request.method == 'POST':
+        numero = request.POST.get('numero_pessoas')
+
+        if numero == '5+':
+            numero = 5
+        else:
+            numero = int(numero)
+
+        Reserva.objects.create(
+            nome=request.POST.get('nome'),
+            data=request.POST.get('data'),
+            hora=request.POST.get('hora'),
+            numero_pessoas=numero
+        )
+    return redirect('home')
